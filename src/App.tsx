4f78539415d0ef997e92a87c41b2f3b571746150
adiNitz <adi.pinhas@nitzanim.tech>
2024-10-07 12:04:39 +0300

    use mui drop zone

--- src/App.tsx
+++ src/App.tsx
@@ -6,17 +6,9 @@ import { FileInput } from './components/FileInput';
 import { ExcellDataProvider } from './contexts';
 
 const App = () => {
-    const student: Student = {
-        firstName: 'ישראל',
-        lastName: 'ישראלי',
-        image: 'https://images.squarespace-cdn.com/content/v1/5e10bdc20efb8f0d169f85f9/09943d85-b8c7-4d64-af31-1a27d1b76698/arrow.png?format=2500w',
-    };
-
     return (
         <ExcellDataProvider>
             <Box>
-                <StudentCard student={{ ...student }} />
-                {/* <NameOption name="יובל בארי"/> */}
                 <FileInput />
             </Box>
         </ExcellDataProvider>
--- src/components/FileInput/FileInput.tsx
+++ src/components/FileInput/FileInput.tsx
@@ -1,76 +1,77 @@
-import Dropzone from 'react-dropzone';
+import { Box, Card, Typography } from '@mui/material';
+import { FC, ReactNode } from 'react';
 import * as XLSX from 'xlsx';
+import { DropzoneArea } from 'mui-file-dropzone';
 import { useExcelDataContext } from '../../hooks';
-import { ChangeEvent, FC } from 'react';
-import { STUDENT_EXCEL_KEYS } from '../../utils/consts';
 import { Student } from '../../types';
-import { Box, Button, Card, Typography } from '@mui/material';
+import { STUDENT_EXCEL_KEYS } from '../../utils/consts';
+import { StudentCard } from '../StudentCard';
 
 export const FileInput: FC = () => {
-    const { data, setData } = useExcelDataContext();
+	const { data, setData } = useExcelDataContext();
 
-    const mapDataToStudents = (data: unknown[]) => {
-        return data.map((element) => {
-            const student: Partial<Student> = {};
+	const mapDataToStudents = (data: unknown[]) => {
+		return data.map((element) => {
+			const student: Partial<Student> = {};
 
-            Object.keys(STUDENT_EXCEL_KEYS).forEach((key) => {
-                student[key as keyof Student] = (element as Partial<Student>)[
-                    STUDENT_EXCEL_KEYS[key as keyof Student] as keyof Student
-                ];
-            });
+			Object.keys(STUDENT_EXCEL_KEYS).forEach((key) => {
+				student[key as keyof Student] = (element as Partial<Student>)[
+					STUDENT_EXCEL_KEYS[key as keyof Student] as keyof Student
+				];
+			});
 
-            return student as Student;
-        });
-    };
+			return student as Student;
+		});
+	};
 
-    const handleFileUpload = (acceptedFiles: File[]) => {
-        if (acceptedFiles.length != 1) {
-            throw new Error('only one file can be selected');
-        }
+	const handleFileUpload = (acceptedFiles: File[]) => {
+		if (acceptedFiles.length == 0) {
+			setData(null);
+			return;
+		}
 
-        const file = acceptedFiles[0];
+		if (acceptedFiles.length != 1) {
+			throw new Error('only one file can be selected');
+		}
 
-        const fileExtentionIndex = file.name.lastIndexOf('.');
-        const fileExtention = file.name.substring(fileExtentionIndex + 1);
+		const file = acceptedFiles[0];
 
-        if (fileExtention != 'xls' && fileExtention != 'xlsx') {
-            throw new Error('file extention must be xls or xlsx');
-        }
+		const fileExtentionIndex = file.name.lastIndexOf('.');
+		const fileExtention = file.name.substring(fileExtentionIndex + 1);
 
-        const reader = new FileReader();
+		if (fileExtention != 'xls' && fileExtention != 'xlsx') {
+			throw new Error('file extention must be xls or xlsx');
+		}
 
-        reader.onload = (event: ProgressEvent<FileReader>) => {
-            const binaryString = event.target?.result;
-            const workbook = XLSX.read(binaryString, { type: 'binary' });
-            const sheetName = workbook.SheetNames[0];
-            const sheet = workbook.Sheets[sheetName];
-            const sheetData = XLSX.utils.sheet_to_json(sheet);
-            const students = mapDataToStudents(sheetData);
+		const reader = new FileReader();
 
-            setData(students);
-        };
+		reader.onload = (event: ProgressEvent<FileReader>) => {
+			const binaryString = event.target?.result;
+			const workbook = XLSX.read(binaryString, { type: 'binary' });
+			const sheetName = workbook.SheetNames[0];
+			const sheet = workbook.Sheets[sheetName];
+			const sheetData = XLSX.utils.sheet_to_json(sheet);
+			const students = mapDataToStudents(sheetData);
 
-        reader.readAsBinaryString(file);
-    };
+			setData(students);
+		};
 
-    return (
-        <Box>
-            <Card>
-                <Button sx={{ width: '50%', height: '50vh' }}>
-                    <Dropzone onDrop={handleFileUpload}>
-                        {({ getRootProps, getInputProps }) => (
-                            <Box {...getRootProps()}>
-                                <input {...getInputProps()} />
-                                <Typography>
-                                    Drag 'n' drop some files here, or click to
-                                    select files
-                                </Typography>
-                            </Box>
-                        )}
-                    </Dropzone>
-                </Button>
-            </Card>
-            {JSON.stringify(data)}
-        </Box>
-    );
+		reader.readAsBinaryString(file);
+	};
+
+	const renderedStudents = () => {
+		return data?.map((student) => {
+			return <StudentCard student={student} />;
+		});
+	};
+
+	return (
+		<Box>
+			{data ? (
+				renderedStudents()
+			) : (
+				<DropzoneArea fileObjects={[]} onChange={handleFileUpload} />
+			)}
+		</Box>
+	);
 };
